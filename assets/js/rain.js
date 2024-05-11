1e2af3275a8016e42444df566ffa27252956d758
картон по жизни <92618443+panwitter@users.noreply.github.com>
2024-05-11 18:27:34 +1000

    лол

--- assets/js/rain.js
+++ assets/js/rain.js
@@ -20,20 +20,41 @@ function RainDrop(x_param, y_param, endY_param, speed_param, opacity_param) {
         c.strokeStyle = `rgba(255, 255, 255, ${this.opacity})`;
         c.stroke();
     };
+    this.updated = function () {
+        let rainEnd = window.innerHeight;
+        if (this.y >= rainEnd) {
+            this.y = this.endY - 100;
+        }
+        else {
+            this.y = this.y + this.speed;
+        }
+        this.draw();
+    }
 }
 
-function random(min=0, max) {
-    return Math.floor(Math.random()*(max - min + 1) + min);
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 let rainDropMass = [];
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 1000; i++) {
     let randomX = random(0, window.innerWidth);
     let randomY = random(0, window.innerHeight);
-   rainDropMass.push(new RainDrop(randomX, randomY, 880, 10, 1));
+    let dropHeight = random(10, 20)
+    let speed = random(1, 10)
+    let opacity = Math.random()
+    rainDropMass.push(new RainDrop(randomX, randomY, dropHeight, speed, opacity));
 }
-rainDropMass.forEach(element => {
-    element.draw();
-});
-console.log(rainDropMass);
+//rainDropMass.forEach((element) => {
+//    element.draw();
+//})
+function animateRain() {
+    requestAnimationFrame(animateRain);
+    c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+    rainDropMass.forEach((drop) => {
+        drop.updated();
+    });
+}
+animateRain();
 
